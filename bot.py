8639ec321d996a82193e488aa8008bd868b51ad3
Nama <mrt690@gmail.com>
2014-02-16 23:54:45 +0100

    -Added Stream Broadcasts -Fixed 'stopping trigger'

--- bot.py
+++ bot.py
@@ -14,9 +14,12 @@ irc.client.ServerConnection.buffer_class.errors = 'ignore'
 config = configparser.ConfigParser()
 config.read('cmds.ini')
 bc = 0
-t = None
-y = None
-wr = None
+wr = 0
+bc_t = False
+bc_y = False
+bc_wr = False
+bc_st = False
+bc_st2 = False
 
 class TestBot(irc.bot.SingleServerIRCBot):
     def __init__(self, channel, nickname, server, port=6667):
@@ -48,13 +51,13 @@ class TestBot(irc.bot.SingleServerIRCBot):
             elif ''.join(a).find('kirika') != -1:
                 if e.target.lower() == '#terraria-support':
                     c.privmsg(e.target, 'If you need help, type !help')
-                else:
+                elif  e.target.lower() != '#terraria':
                     c.privmsg(e.target, config.get('rest', 'contact'))
         except TypeError:
             pass
 
     #Broadcasts ------>
-    def bc_terraria(self, e, bcterraria):
+    def bc_terraria(self, e):
         c = self.connection
         config.read('broadcasts.ini')
         while t.is_alive:
@@ -62,7 +65,7 @@ class TestBot(irc.bot.SingleServerIRCBot):
                 c.privmsg('#terraria', config.get('#terraria', i))
                 sleep(10)
 
-    def bc_yamaria(self, e, bcyamaria):
+    def bc_yamaria(self, e):
         c = self.connection
         config.read('broadcasts.ini')
         while y.is_alive:
@@ -70,11 +73,23 @@ class TestBot(irc.bot.SingleServerIRCBot):
                 c.privmsg('#Yamaria', config.get('#Yamaria', m))
                 sleep(900)
 
-    def bc_worldreset(self, e, bc):
+    def bc_worldreset(self, e):
         c = self.connection
         while wr.is_alive:
-            c.privmsg('#Yamaria', '#10We are preparing the new World. You will be informed, if the server is free to join.' )
+            c.privmsg('#Yamaria', '#10We are preparing the new World. You will be informed, if the server is free to join.')
             sleep(120)
+
+    def bc_stream(self, e, game):
+        c = self.connection
+        while st.is_alive:
+            c.privmsg('#terraria', '10Yama will stream4 %s 10in a few minutes!' %game)
+            sleep(3)
+
+    def bc_stream2(self, e, game):
+        c = self.connection
+        while st2.is_alive:
+            c.privmsg('#terraria', '10Yama is streaming4 %s 10now: 4http://www.twitch.tv/Yamahi' %game)
+            sleep(3)
 
     #Commands ------>
     def do_command(self, e, cmd, bc):
@@ -94,36 +109,85 @@ class TestBot(irc.bot.SingleServerIRCBot):
                 self.disconnect()
             elif cmd == 'exit' and nick == 'Yama':
                 self.die()
+            elif cmd in config.options('admcmd'):
+                c.privmsg('#Yamaria', config.get('admcmd', cmd))
+            elif cmd.split()[0] == 'say':
+                say = ' '.join(cmd.split()[2:])
+                c.privmsg(cmd.split()[1], say)
+
+            #Broadcasts ------>
             elif 'bc' == cmd.split(' ')[0]:
-                if '#terraria' in cmd:
+                if '#terraria' == cmd.split(' ')[1]:
+                    global bc_t, t
                     if 'stop' in cmd:
-                        if t != None and t.is_alive:
+                        if bc_t:
+                            bc_t = False
                             t.terminate()
                             c.privmsg(e.target, '4#Terraria Broadcast halted')
-                    else:
-                        global t
-                        t = Process(target=self.bc_terraria, args=(e, bc))
+                    elif bc_t == False:
+                        bc_t = True
+                        t = Process(target=self.bc_terraria, args=(e,))
                         t.start()
+                    else:
+                        c.privmsg(e.target, '4Broadcast is still running, dood')
 
-                elif '#Yamaria' in cmd:
+                elif '#Yamaria' == cmd.split(' ')[1]:
+                    global bc_y, y
                     if 'stop' in cmd:
-                        if y != None and y.is_alive:
+                        if bc_y:
+                            bc_y = False
                             y.terminate()
                             c.privmsg(e.target, '4#Yamaria Broadcast halted')
-                    else:
-                        global y
-                        y = Process(target=self.bc_yamaria, args=(e, bc))
+                    elif bc_y == False:
+                        bc_y = True
+                        y = Process(target=self.bc_yamaria, args=(e,))
                         y.start()
+                    else:
+                        c.privmsg(e.target, '4Broadcast is still running, dood')
 
                 elif 'world reset' in cmd:
+                    global bc_wr, wr
                     if 'stop' in cmd:
-                        if wr != None and wr.is_alive:
+                        if bc_wr:
+                            bc_wr = False
                             wr.terminate()
                             c.privmsg('#Yamaria', '#4You can join the server now!')
-                    else:
-                        global wr
-                        wr = Process(target=self.bc_worldreset, args=(e, bc))
+                    elif bc_wr == False:
+                        bc_wr = True
+                        wr = Process(target=self.bc_worldreset, args=(e,))
                         wr.start()
+                    else:
+                        c.privmsg(e.target, '4Broadcast is still running, dood')
+
+                elif 'stream' == cmd.split(' ')[1]:
+                    global bc_st, st
+                    if 'stop' in cmd:
+                        if bc_st:
+                            bc_st = False
+                            st.terminate()
+                            c.privmsg(e.target, '4Stream-Broadcast halted')
+                    elif bc_st == False:
+                        bc_st = True
+                        game = ' '.join(cmd.split(' ')[2:])
+                        st = Process(target=self.bc_stream, args=(e, game))
+                        st.start()
+                    else:
+                        c.privmsg(e.target, '4Broadcast is still running, dood')
+
+                elif 'stream2' == cmd.split(' ')[1]:
+                    global bc_st2, st2
+                    if 'stop' in cmd:
+                        if bc_st2:
+                            bc_st2 = False
+                            st2.terminate()
+                            c.privmsg(e.target, '4Stream2-Broadcast halted')
+                    elif bc_st2 == False:
+                        bc_st2 = True
+                        game = ' '.join(cmd.split(' ')[2:])
+                        st2 = Process(target=self.bc_stream2, args=(e, game))
+                        st2.start()
+                    else:
+                        c.privmsg(e.target, '4Broadcast is still running, dood')
 
 def main():
     import sys
